23cd74fa735bd1314f6273cd13ac6d91284607d4
ritu-477 <ritujoharbishnoi@gmail.com>
2025-01-18 14:29:14 +0530

    done

--- src/components/TextWithImage.jsx
+++ src/components/TextWithImage.jsx
@@ -4,16 +4,19 @@ const TextWithImage = () => {
 
   return (
     <div className="flex justify-center items-center bg-gray-500 h-screen flex-col">
-      <h1 className="text-7xl mb-10">Text With Image And Typewriter Effect</h1>
-      <h2 className="text-8xl font-extrabold bg-clip-text text-transparent pt-10 bg-cover bg-center bg-india-flag">
-        <ReactTyped
-          strings={["INDIA"]} 
-          typeSpeed={150} 
-          backSpeed={50} 
-          backDelay={1000} 
-          loop={true} 
-        />
-      </h2>
+      <div className="container mx-auto">
+        <h1 className="lg:text-6xl text-4xl mb-5 lg:mb-10 text-center">Text With Image And Typewriter Effect</h1>
+        <h2 className="text-8xl text-center font-extrabold bg-clip-text text-transparent pt-10 bg-no-repeat bg-cover bg-center bg-india-flag">
+          <ReactTyped
+            className="text-center"
+            strings={["INDIA"]}
+            typeSpeed={150}
+            backSpeed={50}
+            backDelay={1000}
+            loop={true}
+          />
+        </h2>
+     </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "20px",
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1180px",
+        },
+      },
+
       fontSize: {
         'custom-xl': "200px",
       },
