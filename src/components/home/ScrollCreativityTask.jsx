f43ecd878677082722b16f1d87421c23ab691cd7
ritu-477 <ritujoharbishnoi@gmail.com>
2025-02-03 09:55:22 +0530

    remove extra code

--- src/components/home/ScrollCreativityTask.jsx
+++ src/components/home/ScrollCreativityTask.jsx
@@ -47,9 +47,7 @@ const ScrollCreativityTask = () => {
     return (
         <div className='bg-black py-5'>
             <div className='container'>
-                <div className="spacer text-center">
                     <h2 className="lg:text-6xl text-white text-4xl mb-5 lg:mb-10 text-center font-semibold">Scroll Trigger Gsap Animation</h2>
-                </div>
                 <div className="main relative min-h-screen">
                     <div className="initial absolute sm:size-[140px] size-24 flex items-center justify-center border-2 border-dashed border-white rounded-lg left-[60%] top-[10%]">
                         <div className="box bg-gradient-to-r from-blue-500 to-purple-600 sm:size-[100px] size-[70px] relative rounded-lg z-10" />
@@ -61,7 +59,7 @@ const ScrollCreativityTask = () => {
                         <div className="marker sm:size-[100px] size-[70px] bg-gradient-to-r from-yellow-400 to-orange-600 rounded-lg" />
                     </div>
                 </div>
-                <div className="spacer final"></div>
+                <div className="final"></div>
             </div>
         </div>
     );
