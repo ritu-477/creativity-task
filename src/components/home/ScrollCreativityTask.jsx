d0bbcdf639560ca441481019c4bebb5c299b8bd6
ritu-477 <ritujoharbishnoi@gmail.com>
2025-02-03 09:29:10 +0530

    add container

--- src/components/home/ScrollCreativityTask.jsx
+++ src/components/home/ScrollCreativityTask.jsx
@@ -6,12 +6,11 @@ gsap.registerPlugin(Flip, ScrollTrigger);
 
 const ScrollCreativityTask = () => {
     useEffect(() => {
-        let flipCtx;
-
+        let flipContext;
         const createTimeline = () => {
-            flipCtx && flipCtx.revert();
+            flipContext && flipContext.revert();
 
-            flipCtx = gsap.context(() => {
+            flipContext = gsap.context(() => {
                 const secondState = Flip.getState('.second .marker');
                 const thirdState = Flip.getState('.third .marker');
                 const flipConfig = {
@@ -21,7 +20,7 @@ const ScrollCreativityTask = () => {
 
                 const tl = gsap.timeline({
                     scrollTrigger: {
-                        trigger: '.container.initial',
+                        trigger: '.initial',
                         start: 'clamp(top center)',
                         endTrigger: '.final',
                         end: 'clamp(top center)',
@@ -47,21 +46,23 @@ const ScrollCreativityTask = () => {
 
     return (
         <div className='bg-black py-5'>
-            <div className="spacer text-center">
-                <h2 className="lg:text-6xl text-white text-4xl mb-5 lg:mb-10 text-center font-semibold">Scroll Trigger Gsap Animation</h2>
+            <div className='container'>
+                <div className="spacer text-center">
+                    <h2 className="lg:text-6xl text-white text-4xl mb-5 lg:mb-10 text-center font-semibold">Scroll Trigger Gsap Animation</h2>
+                </div>
+                <div className="main relative min-h-screen">
+                    <div className="initial absolute sm:size-[140px] size-24 flex items-center justify-center border-2 border-dashed border-white rounded-lg left-[60%] top-[10%]">
+                        <div className="box bg-gradient-to-r from-blue-500 to-purple-600 sm:size-[100px] size-[70px] relative rounded-lg z-10" />
+                    </div>
+                    <div className="second absolute sm:size-[140px] size-24 flex items-center justify-center border-2 border-dashed border-white rounded-lg left-[10%] top-[50%]">
+                        <div className="marker sm:size-[100px] size-[70px] bg-gradient-to-r from-green-400 to-blue-500 rounded-lg" />
+                    </div>
+                    <div className="third absolute sm:size-[140px] size-24 flex items-center justify-center border-2 border-dashed border-white rounded-lg right-[10%] bottom-[3rem]">
+                        <div className="marker sm:size-[100px] size-[70px] bg-gradient-to-r from-yellow-400 to-orange-600 rounded-lg" />
+                    </div>
+                </div>
+                <div className="spacer final"></div>
             </div>
-            <div className="main relative min-h-screen">
-                <div className="initial absolute sm:size-[140px] size-24 flex items-center justify-center border-2 border-dashed border-white rounded-lg left-[60%] top-[10%]">
-                    <div className="box bg-gradient-to-r from-blue-500 to-purple-600 sm:size-[100px] size-[70px] relative rounded-lg z-10" />
-                </div>
-                <div className="second absolute sm:size-[140px] size-24 flex items-center justify-center border-2 border-dashed border-white rounded-lg left-[10%] top-[50%]">
-                    <div className="marker sm:size-[100px] size-[70px] bg-gradient-to-r from-green-400 to-blue-500 rounded-lg" />
-                </div>
-                <div className="third absolute sm:size-[140px] size-24 flex items-center justify-center border-2 border-dashed border-white rounded-lg right-[10%] bottom-[3rem]">
-                    <div className="marker sm:size-[100px] size-[70px] bg-gradient-to-r from-yellow-400 to-orange-600 rounded-lg" />
-                </div>
-            </div>
-            <div className="spacer final"></div>
         </div>
     );
 };
